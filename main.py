dd03df3cca11567301d78a13c27c49dbaccbec40
ayan584 <s98227636@gmail.com>
2025-06-12 19:57:12 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+num1 = int(input("Биринчи санды жазыныз: "))
+num2 = int(input("Экинчи санды жазыныз: "))
+op = input(" Операцияны танданыз (+, -, *, / :")
+
+
+if op == "+":
+    ans = num1 + num2
+    print("Жооп: " + str(ans))
+
+
+
+elif op == "-":
+    ans = num1 - num2
+    print("Жооп: " + str(ans))
+
+elif op == "*":
+    ans = num1 * num2
+    print("Жооп: " + str(ans))
+
+elif op == "/":
+    if num2 == 0:
+        print("Нолго болунбойт!")
+    else:
+        ans = num1/num2
+        print("Жооп: " + str(ans))
+else:
+    print("Андай операция жок!")
